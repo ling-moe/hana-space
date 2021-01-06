df1c46c9495dc76231438a25e43ac35c372668cc
yukdawn <yukdawn@gmail.com>
2021-01-06 17:19:44 +0800

    [FIX]: 实现checkbox

--- projects/hana-ui/src/lib/seed/checkbox/checkbox.component.ts
+++ projects/hana-ui/src/lib/seed/checkbox/checkbox.component.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import {EventEmitter, HostListener} from '@angular/core';
+import {ChangeDetectorRef, Component, ElementRef, forwardRef, Input, OnInit, Output, Renderer2, TemplateRef} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
-  selector: 'hana-checkbox',
+  selector: 'label[hana-checkbox]',
   templateUrl: './checkbox.component.html',
-  styleUrls: ['./checkbox.component.scss']
+  styleUrls: ['./checkbox.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CheckboxComponent),
+      multi: true
+    }
+  ]
 })
-export class CheckboxComponent implements OnInit {
+export class CheckboxComponent implements OnInit, ControlValueAccessor {
 
-  constructor() { }
+  @Input()
+  checked = false;
 
-  ngOnInit(): void {
+  @Input()
+  disabled = false;
+
+  @Input()
+  checkedIcon: TemplateRef<HTMLElement>;
+
+  @Input()
+  unCheckedIcon: TemplateRef<HTMLElement>;
+
+  @Output() readonly nzCheckedChange = new EventEmitter<boolean>();
+
+  onChange: (checked: boolean) => {};
+  onTouch: () => {};
+
+  constructor(
+    private el: ElementRef,
+    private cdr: ChangeDetectorRef,
+    private render: Renderer2
+  ) { }
+
+  writeValue(value: boolean): void {
+    this.checked = value;
+    this.cdr.markForCheck();
   }
 
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+    this.cdr.markForCheck();
+  }
+
+  ngOnInit(): void {
+    const checkBoxClass = ['hana-checkbox'];
+    if (this.checked){
+      checkBoxClass.push('hana-checkbox-checked');
+    }
+    if (this.disabled){
+      checkBoxClass.push('hana-checkbox-disabled');
+    }
+    this.render.setAttribute(this.el, 'class', checkBoxClass.join(' '));
+  }
+
+
+  @HostListener('click')
+  hostClick(e: MouseEvent): void {
+    e.preventDefault();
+    this.innerCheckedChange(!this.checked);
+  }
+
+  innerCheckedChange(checked: boolean): void {
+    if (!this.disabled) {
+      this.checked = checked;
+      this.onChange(this.checked);
+      this.nzCheckedChange.emit(this.checked);
+    }
+  }
 }
--- projects/hana-ui/src/lib/seed/checkbox/checkbox.module.ts
+++ projects/hana-ui/src/lib/seed/checkbox/checkbox.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckboxComponent } from './checkbox.component';
+import {FormsModule} from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [CheckboxComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class CheckboxModule { }
